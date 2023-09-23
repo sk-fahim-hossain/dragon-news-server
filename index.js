8a26875b20ea81eadc434e8851c0b0ecf09da2cc
Fahim Hossain <67559866+sk-fahim-hossain@users.noreply.github.com>
2023-09-23 19:13:40 +0600

    basic server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+
 app.use(cors())
+
 
 const categories = require('./data/categories')
 
