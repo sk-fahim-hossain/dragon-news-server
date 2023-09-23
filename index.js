9708668eb55f718a3fe6738783e68b9a53c6f42e
Fahim Hossain <67559866+sk-fahim-hossain@users.noreply.github.com>
2023-09-24 05:05:44 +0600

    server created succesfully

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 
 const categories = require('./data/categories')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
   res.send('SERVER IS Running')
@@ -14,6 +15,29 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req,res) =>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req,res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id == id);
+    res.send(selectedNews)
+    console.log(selectedNews)
+})
+
+app.get('/categories/:id', (req,res) =>{
+    const id = parseInt(req.params.id)
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const selectedCategory = news.filter(c => parseInt(c.category_id) === id)
+        res.send(selectedCategory)
+    }
+   
 })
 
 app.listen(port, () => {
